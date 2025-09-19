000caaa449ebfef0f42e01879c80688af27f5acc
Marc Isaac <marc_isaac@outlook.fr>
2025-09-19 18:35:50 +0200

    simplification fu formulaire et traitement dans fichier dédié

--- ajouter.php
+++ ajouter.php
@@ -1,38 +1,11 @@
 <?php require 'header.php'; ?>
 <?php require 'bdd.php'; ?>
 <?php require 'functions.php'; ?>
-<?php
-// Traitement du formulaire d'ajout des oeuvres
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $titre = $_POST['titre'];
-    $artiste = $_POST['artiste'];
-    $image = $_POST['image'];
-    $description = $_POST['description'];
-   
-
-    // Ajouter le produit
-    $result = ajouterOeuvre($titre, $artiste, $image, $description);
-
-    if ($result) {
-        $success = "Oeuvre ajouté avec succès.";
-    } else {
-        $error = "Erreur lors de l'ajout du Oeuvre.";
-    }
-}
 
 
 
 
-?>
-
-<!-- Affichage des messages -->
-    <?php if (isset($success)): ?>
-        <div class="alert alert-success"><?= htmlspecialchars($success) ?></div>
-    <?php elseif (isset($error)): ?>
-        <div class="alert alert-danger"><?= htmlspecialchars($error) ?></div>
-    <?php endif; ?>
-
-<form action="" method="POST">
+<form action="traitement.php" method="POST">
     <div class="champ-formulaire">
         <label for="titre">Titre de l'œuvre</label>
         <input type="text" name="titre" id="titre">
